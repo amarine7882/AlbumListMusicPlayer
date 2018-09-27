7e36001872d1de4289d42c1296e330adea82d678
Andrew Marine <andrewmarine7882@gmail.com>
2018-09-26 17:22:27 -0700

    converted to promises, added post, put and delete handlers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { getArtist } = require('../database/index.js');
 const cors = require('cors');
+const db = require('../database/index.js');
+
 const server = express();
 
 server.use(bodyParser.json());
@@ -10,10 +11,28 @@ server.use(cors());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
-server.get('/artists/albums/:artistID', (req, res) => {
-  getArtist(req.params.artistID, data => {
-    res.send(data);
-  });
+server.get('/artists/:artistID/albums', (req, res) => {
+  db.getArtist(req.params.artistID)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+});
+
+server.post('/artists', (req, res) => {
+  db.insertArtists(req.params.artists)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+});
+
+server.put('/artists/:artistID', (req, res) => {
+  db.updateArtist(req.params.artistID)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+});
+
+server.delete('artists/:artistID', (req, res) => {
+  db.deleteArtist(req.params.artistID)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
 });
 
 module.exports = server;
