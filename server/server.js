8776d4e275d0777f97ad6b4fb50975e86987bc12
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-05 09:50:14 -0700

    refactored body parsing to use express 4.x built in method and be called only in requests where needed

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const db = require('../database/index.js');
 
 const server = express();
 
-server.use(bodyParser.json());
 server.use(cors());
-server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.get('/artists/:artistID/albums', (req, res) => {
@@ -17,13 +14,13 @@ server.get('/artists/:artistID/albums', (req, res) => {
     .catch(err => console.log(err));
 });
 
-server.post('/:type', (req, res) => {
-  db.createRecord(req.body, req.params.type)
+server.post('/:type', express.json(), (req, res) => {
+  db.createRecord(req.body.data, req.params.type)
     .then(data => res.send(data))
     .catch(err => console.log(err));
 });
 
-server.put('/:type/:id', (req, res) => {
+server.put('/:type/:id', express.json(), (req, res) => {
   db.updateRecord(req.params.id, req.body, req.params.type)
     .then(data => res.send(data))
     .catch(err => console.log(err));
