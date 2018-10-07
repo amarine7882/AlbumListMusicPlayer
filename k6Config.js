c1829ab28e25702e3763ebfa88506ed24b2817da
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-06 17:57:04 -0700

    Removed clustering

--- k6Config.js
+++ k6Config.js
@@ -1,15 +1,24 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
-  vus: 700,
-  rps: 3000,
-  duration: '30s',
+  vus: 500,
+  rps: 1600,
+  duration: '1m',
 };
 
 export default function () {
-  const res = http.get('http://localhost:3001/artists/6/albums');
+  for (let i = 0; i < 3; i += 1) {
+    const res = http.get(`http://localhost:3001/artists/${i + 2000000}/albums`);
+    check(res, {
+      'status was 200': r => r.status === 200,
+      'transaction time OK': r => r.timings.duration < 800,
+    });
+  }
+  const url = `http://localhost:3001/artists/${Math.floor(Math.random() * 9000000) + 1}/albums`;
+  const res = http.get(url);
   check(res, {
     'status was 200': r => r.status === 200,
+    'transaction time OK': r => r.timings.duration < 800,
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-require('newrelic');
-const cluster = require('cluster');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -7,52 +5,41 @@ const http = require('http');
 const tooBusy = require('toobusy-js');
 const db = require('../database/index.js');
 
-if (cluster.isMaster) {
-  for (let i = 0; i < 8; i += 1) {
-    cluster.fork();
+http.globalAgent.maxSockets = 100;
+const server = express();
+
+server.use(cors());
+server.use((req, res, next) => {
+  if (tooBusy()) {
+    res.status(503).send('Oops, server is busy');
+  } else {
+    next();
   }
-} else {
-  http.globalAgent.maxSockets = 100;
-  const server = express();
-
-  server.use(cors());
-  server.use((req, res, next) => {
-    if (tooBusy()) {
-      res.status(503).send('Oops, server is busy');
-    } else {
-      next();
-    }
-  });
-  server.use(express.static(path.join(__dirname, '../public')));
-
-  server.get('/artists/:artistID/albums', (req, res) => {
-    db.getDataForArtist(req.params.artistID)
-      .then(data => res.status(200).send(data))
-      .catch(err => console.log(err));
-  });
-
-  server.post('/:type', express.json(), (req, res) => {
-    db.createRecord(req.body, req.params.type)
-      .then(data => res.send(data))
-      .catch(err => console.log(err));
-  });
-
-  server.put('/:type/:id', express.json(), (req, res) => {
-    db.updateRecord(req.params.id, req.body, req.params.type)
-      .then(data => res.send(data))
-      .catch(err => console.log(err));
-  });
-
-  server.delete('/:type/:id', (req, res) => {
-    db.deleteRecord(req.params.id, req.params.type)
-      .then(data => res.send(data))
-      .catch(err => console.log(err));
-  });
-
-  server.listen(3001, () => console.log('Listening on port 3001'));
-}
-
-cluster.on('exit', (worker) => {
-  console.log(worker.id, ' crashed');
-  cluster.fork();
 });
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.get('/artists/:artistID/albums', (req, res) => {
+  db.getDataForArtist(req.params.artistID)
+    .then(data => res.status(200).send(data))
+    .catch(err => console.log(err));
+});
+
+server.post('/:type', express.json(), (req, res) => {
+  db.createRecord(req.body, req.params.type)
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
+});
+
+server.put('/:type/:id', express.json(), (req, res) => {
+  db.updateRecord(req.params.id, req.body, req.params.type)
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
+});
+
+server.delete('/:type/:id', (req, res) => {
+  db.deleteRecord(req.params.id, req.params.type)
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
+});
+
+server.listen(3001, () => console.log('Listening on port 3001'));
